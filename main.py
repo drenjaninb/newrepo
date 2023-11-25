134804f2540b6e72c926338e504f37871c9f4be9
Boris.Drenjanin <drenjaninb@gmail.com>
2023-11-25 22:49:05 +0100

    1st commit

---
+++ main.py
@@ -0,0 +1,15 @@
+class Wagen:
+    def start (self):
+        print("Engine ON.")
+    def shut_down(self):
+        print("Engine Off.")
+
+class Mazda(Wagen):
+    def __init__(self,modellbezeichnung, volumen, kw, farbe):
+        self.modellbezeichnung = modellbezeichnung
+        self.volumen = volumen
+        self.kw = kw
+        self.farbe = farbe
+
+mazdacar = Mazda(3,2000,88,'soul red')
+print(mazdacar.start())
